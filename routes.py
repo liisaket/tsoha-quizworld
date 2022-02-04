2df9c0e70b32f9573133f296fbd82640351357ea
liisaket <95650016+liisaket@users.noreply.github.com>
2022-02-05 01:14:49 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,13 @@
 from app import app
-from flask import render_template, request, redirect, session
+from flask import render_template, request, redirect, session, abort
 import users
+import quizzes
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    nmr_of_quizzes = quizzes.get_nmr_of_quizzes()
+    available_quizzes = quizzes.get_quizzes()
+    return render_template("index.html", nmr_of_quizzes=nmr_of_quizzes, available_quizzes=available_quizzes)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -48,3 +51,23 @@ def register():
         if not users.register(username, password1, role):
             return render_template("error.html", message="Rekisteröinti ei onnistunut", route="/register")
         return redirect("/")
+    
+@app.route("/base")
+def base():
+    if not users.require_role(2):
+        return render_template("error.html", message="Sinulla ei ole oikeuksia luoda kyselyitä", route="/")
+    return render_template("base.html")
+
+@app.route("/new", methods=["POST"])
+def new():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
+    if not users.require_role(2):
+        return render_template("error.html", message="Sinulla ei ole oikeuksia luoda kyselyitä", route="/base")
+    topic = request.form["topic"]
+    if topic == "" or len(topic) > 20:
+        return render_template("error.html", message="Kyselyn aiheessa tulee olla 1-20 merkkiä", route="/base")
+    quiz_type = int(request.form["quiz_type"])
+    nmr_of_questions = int(request.form["nmr_of_questions"])
+    nmr_of_choices = int(request.form["nmr_of_choices"])
+    return render_template("newquiz.html", topic=topic, quiz_type=quiz_type, questions=nmr_of_questions, choices=nmr_of_choices)
