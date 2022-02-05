d9645e1e57b87642840ff8855158252371e0dd55
liisaket <95650016+liisaket@users.noreply.github.com>
2022-02-05 16:08:34 +0200

    Add create-quiz functions to quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -1,4 +1,5 @@
 from db import db
+import users
 
 def get_quizzes():
     sql = "SELECT topic, id, quiz_type FROM quizzes"
@@ -9,3 +10,20 @@ def get_nmr_of_quizzes():
     sql = "SELECT COUNT(*) FROM quizzes"
     result = db.session.execute(sql)
     return result.fetchone()[0]
+
+def create_quiz(topic, quiz_type):
+    sql = "INSERT INTO quizzes (topic, quiz_type) VALUES (:topic, :quiz_type) RETURNING id"
+    result = db.session.execute(sql, {"topic":topic,"quiz_type":quiz_type})
+    db.session.commit()
+    return result.fetchone()[0]
+
+def create_question(quiz_id, question):
+    sql = "INSERT INTO questions (content, quiz_id) VALUES (:content, :quiz_id) RETURNING id"
+    result = db.session.execute(sql, {"content":question, "quiz_id":quiz_id})
+    db.session.commit()
+    return result.fetchone()[0]
+
+def create_choice(question_id, choice, correct):
+    sql = "INSERT INTO choices (question_id, content, correct) VALUES (:question_id, :content, :correct)"
+    db.session.execute(sql, {"question_id":question_id, "content":choice, "correct":correct})
+    db.session.commit()
