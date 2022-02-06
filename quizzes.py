e9fbe5ff9bbfe4a71f134279edd3a0f9693b5d9e
liisaket <95650016+liisaket@users.noreply.github.com>
2022-02-06 21:37:56 +0200

    Update quizzes.py

--- quizzes.py
+++ quizzes.py
@@ -6,10 +6,74 @@ def get_quizzes():
     result = db.session.execute(sql)
     return result.fetchall()
 
+def get_topics():
+    sql = "SELECT topic FROM quizzes"
+    result = db.session.execute(sql)
+    return [row[0] for row in result.fetchall()]
+
 def get_nmr_of_quizzes():
     sql = "SELECT COUNT(*) FROM quizzes"
     result = db.session.execute(sql)
     return result.fetchone()[0]
+
+def get_quiz_topic(id):
+    sql = "SELECT topic FROM quizzes WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchone()[0]
+
+def get_quiz_type(id):
+    sql = "SELECT quiz_type FROM quizzes WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchone()[0]
+
+def get_questions(id):
+    sql = "SELECT id, content FROM questions where quiz_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchall()
+
+def get_choices(questions):
+    question_ids = [question[0] for question in questions]
+    sql = "SELECT id, content, question_id, correct FROM choices WHERE question_id IN :questions"
+    result = db.session.execute(sql, {"questions":tuple(question_ids)})
+    return result.fetchall()
+
+def store_user_answers(choice_ids):
+    user = users.user_id()
+    for choice in choice_ids:
+        sql = "INSERT INTO answers (user_id, choice_id) VALUES (:user, :choice)"
+        db.session.execute(sql, {"user":user, "choice":choice})
+    db.session.commit()
+
+def get_user_answers(id):
+    user = users.user_id()
+    sql = "SELECT c.id FROM choices c JOIN answers a ON c.id=a.choice_id \
+        JOIN users u ON a.user_id=u.id \
+        JOIN questions q ON c.question_id=q.id \
+        JOIN quizzes qz ON q.quiz_id=qz.id \
+        WHERE qz.id=:id AND u.id=:user"
+    result = db.session.execute(sql, {"id":id, "user":user})
+    return [row[0] for row in result.fetchall()]
+
+def user_right_answers(id):
+    user = users.user_id()
+    sql = "SELECT COUNT(c.id) FROM choices c \
+        JOIN answers a ON c.id=a.choice_id \
+        JOIN users u ON a.user_id=u.id \
+        JOIN questions q ON c.question_id=q.id \
+        JOIN quizzes qz ON q.quiz_id=qz.id \
+        WHERE qz.id=:id AND u.id=:user AND c.correct"
+    result = db.session.execute(sql, {"id":id, "user":user})
+    return result.fetchone()[0]
+
+def get_poll_choices(id):
+    sql = "SELECT c.id, c.question_id, c.content, COUNT(a.id) FROM choices c \
+        LEFT JOIN answers a ON c.id=a.choice_id \
+        LEFT JOIN users u ON a.user_id=u.id \
+        LEFT JOIN questions q ON c.question_id=q.id \
+        LEFT JOIN quizzes qz ON q.quiz_id=qz.id \
+        WHERE qz.id=:id GROUP BY c.id ORDER BY c.id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchall()
 
 def create_quiz(topic, quiz_type):
     sql = "INSERT INTO quizzes (topic, quiz_type) VALUES (:topic, :quiz_type) RETURNING id"
