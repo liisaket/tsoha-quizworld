fda54bf2488ff5344a0684823f6c3bfae24b7222
Liisa <95650016+liisaket@users.noreply.github.com>
2023-04-17 19:13:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import routes
 from os import getenv
 from flask import Flask
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = getenv("SECRET_KEY")
+
+import routes
