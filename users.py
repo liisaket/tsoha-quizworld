0adb5aebcf2027e82ba6a8cedfd805bb449d15d1
Liisa <95650016+liisaket@users.noreply.github.com>
2023-04-17 19:50:45 +0300

    Update users.py

--- users.py
+++ users.py
@@ -3,14 +3,13 @@ from flask import abort, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 
-
-def login(username, password):
+def login(username,password):
     sql = "SELECT password, id, role FROM users WHERE username=:username"
-    result = db.session.execute(sql, {"username": username})
+    result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
-    if user is None:
+    if user == None:
         return False
-    if check_password_hash(user[0], password):
+    if check_password_hash(user[0],password):
         session["user_id"] = user[1]
         session["username"] = username
         session["role"] = user[2]
@@ -18,78 +17,66 @@ def login(username, password):
         return True
     return False
 
-
 def logout():
     del session["user_id"]
     del session["username"]
     del session["role"]
     del session["csrf_token"]
 
-
 def register(name, password, role):
     hash_value = generate_password_hash(password)
-    try:
+    try: 
         sql = "INSERT INTO users (username,password,role,created_at) VALUES (:username,:password,:role, NOW())"
-        db.session.execute(
-            sql, {"username": name, "password": hash_value, "role": role})
+        db.session.execute(sql, {"username":name,"password":hash_value,"role":role})
         db.session.commit()
-    except BaseException:
+    except:
         return False
-    return login(name, password)
-
+    return login(name,password)
 
 def delete_user():
     user = user_id()
     sql = "DELETE FROM answers WHERE user_id=:user"
-    db.session.execute(sql, {"user": user})
+    db.session.execute(sql, {"user":user})
     db.session.commit()
     sql = "DELETE FROM users WHERE id=:user"
-    db.session.execute(sql, {"user": user})
+    db.session.execute(sql, {"user":user})
     db.session.commit()
     logout()
-
 
 def check_username(username):
     if len(username) < 1 or len(username) > 20:
         return False
     return True
 
-
 def check_password(password1):
     if password1 == "":
         return False
     return True
-
 
 def check_passwords(password1, password2):
     if password1 != password2:
         return False
     return True
 
-
 def check_role(role):
     if role not in ("1", "2"):
         return False
     return True
 
-
 def user_id():
     return session.get("user_id")
-
 
 def registration_time():
     user = user_id()
     sql = "SELECT created_at FROM users WHERE users.id=:user"
-    result = db.session.execute(sql, {"user": user})
+    result = db.session.execute(sql, {"user":user})
     return result.fetchone()[0]
-
 
 def require_role(role):
     user_role = session["role"]
     if user_role != role:
         return False
     return True
-
 
 def check_csrf():
     if session["csrf_token"] != request.form["csrf_token"]:
